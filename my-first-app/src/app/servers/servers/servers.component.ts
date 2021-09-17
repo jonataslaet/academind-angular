36981d87a146c608e539af1f44362b4e745e11f3
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-09-17 07:11:55 -0300

    Two-Way-Databinding

--- my-first-app/src/app/servers/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers/servers.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  serverName = '';
+  serverName = 'Initial name';
   allowNewServer = false;
   statusCreationServer = 'No server was created';
 
