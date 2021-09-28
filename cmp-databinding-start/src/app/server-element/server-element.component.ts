8ebd0d254bf59dc4cb65c55ff27290b69869ee92
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-09-28 08:17:35 -0300

    Lifecycle Hooks and Template Access - ngAfterViewInit

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,17 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterViewInit {
 
   @Input() element: {type: string, name: string, content: string};
+
+  @ViewChild('heading', {static: true}) head : ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Text content: ' + this.head.nativeElement.textContent);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Text content: ' + this.head.nativeElement.textContent);
   }
 
 }
