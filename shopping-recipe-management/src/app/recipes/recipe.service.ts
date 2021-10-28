604a7c9e4a8040b05453726825601b466f55c11f
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-10-27 23:54:20 -0300

    Adding Ingredients to Recipes

--- shopping-recipe-management/src/app/recipes/recipe.service.ts
+++ shopping-recipe-management/src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
@@ -6,9 +7,22 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe 1', 'A test very good, uhrruh', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe('A test recipe 2', 'A test very good, uhrruh', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe('A test recipe 3', 'A test very good, uhrruh', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+    new Recipe(
+      'A test recipe 1',
+      'A test very good, uhrruh',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+      [new Ingredient('Meat',1), new Ingredient('Meat',1)]),
+    new Recipe(
+      'A test recipe 2',
+      'A test very good, uhrruh',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+      [new Ingredient('Meat',1), new Ingredient('Meat',2)]
+      ),
+    new Recipe(
+      'A test recipe 3',
+      'A test very good, uhrruh',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+      [new Ingredient('Meat',1), new Ingredient('Meat',3)])
   ];
 
   getRecipes() {
